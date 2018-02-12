0bfabdc9a3b930dc1b0e710804dbea242ca2b79c
Nasir Mohammad <nasir@sourceapp.io>
2018-02-12 16:28:34 -0500

    Add simple start / stop

--- src/app/body/snake-game/models/snake.model.ts
+++ src/app/body/snake-game/models/snake.model.ts
@@ -2,6 +2,23 @@ export class Snake {
   constructor (
     public positionX: number,
     public positionY: number,
+    public direction: number = 1,
     public tail: Snake = null
   ) { }
+
+  moveLeft() {
+    this.positionX -= 20;
+  }
+
+  moveRight() {
+    this.positionX += 20;
+  }
+
+  moveUp() {
+    this.positionY -= 20;
+  }
+
+  moveDown() {
+    this.positionY += 20;
+  }
 }
--- src/app/body/snake-game/snake-game.component.ts
+++ src/app/body/snake-game/snake-game.component.ts
@@ -11,12 +11,12 @@ import { HostListener } from '@angular/core';
 export class SnakeGameComponent implements OnInit {
   snake: Snake;
   game: SnakeGame;
+  started: boolean;
 
   constructor() {
-    this.snake = new Snake(400, 240);
-    this.game = new SnakeGame(800, 500, 0, 1, this.snake);
-
-    console.log(this.snake, this.game);
+    this.snake = new Snake(400, 240, 3);
+    this.game = new SnakeGame(800, 500, 0, 10, this.snake);
+    this.started = false;
   }
 
   @HostListener('document:keypress', ['$event'])
@@ -26,30 +26,80 @@ export class SnakeGameComponent implements OnInit {
 
     switch (event.key) {
       case 's':
-        if (this.snake.positionY !== (h - 20)) {
-          this.snake.positionY += 20;
+        if (this.snake.direction !== 2) {
+          this.snake.direction = 1;
         }
         break;
       case 'w':
-        if (this.snake.positionY !== 0) {
-          this.snake.positionY -= 20;
+        if (this.snake.direction !== 1) {
+          this.snake.direction = 2;
         }
         break;
       case 'a':
-        if (this.snake.positionX !== 0) {
-          this.snake.positionX -= 20;
+        if (this.snake.direction !== 4) {
+          this.snake.direction = 3;
         }
         break;
       case 'd':
-        if (this.snake.positionX !== (w - 20)) {
-          this.snake.positionX += 20;
+        if (this.snake.direction !== 3) {
+          this.snake.direction = 4;
         }
         break;
     }
   }
 
   startGame(): void {
-    console.log('game starting...');
+    if (!this.started) {
+      console.log('game starting...');
+      this.started = true;
+
+      if (this.started) {
+        startMoving(this);
+      }
+    }
+
+
+    function startMoving(game) {
+      if (game.started) {
+        setTimeout(function () {
+          game.moveSnake(game.snake.direction);
+          startMoving(game);
+        }, 500 / game.game.speed);
+      }
+    }
+  }
+
+  stopGame(): void {
+    this.started = false;
+  }
+
+  // moves the snake in direction
+  moveSnake(direction: number) {
+    const w = this.game.boardWidth,
+          h = this.game.boardHeight;
+
+    switch (direction) {
+      case 1:
+        if (this.snake.positionY !== (h - 20)) {
+          this.snake.moveDown();
+        }
+        break;
+      case 2:
+        if (this.snake.positionY !== 0) {
+          this.snake.moveUp();
+        }
+        break;
+      case 3:
+        if (this.snake.positionX !== 0) {
+          this.snake.moveLeft();
+        }
+        break;
+      case 4:
+        if (this.snake.positionX !== (w - 20)) {
+          this.snake.moveRight();
+        }
+        break;
+    }
   }
 
   ngOnInit() {
